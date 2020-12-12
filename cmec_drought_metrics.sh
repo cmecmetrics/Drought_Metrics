03ab23eb2807981c9fc69c2f05d8bfc403f5a438
Ana Ordonez <ordonez4@llnl.gov>
2020-12-11 16:23:34 -0800

    change settings

--- cmec_drought_metrics.sh
+++ cmec_drought_metrics.sh
@@ -3,10 +3,12 @@
 # Change file names as needed for region, obs, wgt, and shp.
 # See settings.json for more information about input data.
 # Optional settings are commented out and not used for this example
-#interpolation=True
+intrp=True
 region="New England Region"
+#obspr="precip"
 obspath=$CMEC_OBS_DATA/precip.V1.0.mon.mean.pr.nc
-#wgtpath=$CMEC_MODEL_DATA/interpolated_pr_Amon_E3SM-1-1_historical_r1i1p1f1_gr_187001-200912.nc
+#obspath=$CMEC_OBS_DATA/obs_precip_2.nc
+wgtpath=$CMEC_MODEL_DATA/interpolated_pr_Amon_E3SM-1-1_historical_r1i1p1f1_gr_187001-200912.nc
 shppath=$CMEC_OBS_DATA/HU/WBDHU2.shp
 testpath=$CMEC_MODEL_DATA/
 outpath=$CMEC_WK_DIR/
@@ -14,7 +16,7 @@ logpath=${outpath}/drought_metrics_log.txt
 
 cd $CMEC_WK_DIR
 echo "Running drought metrics"
-python $CMEC_CODE_DIR/drought_metrics.py -test_path $testpath -obs_path $obspath -hu_name "$region" -shp_path $shppath -out_path $outpath >> $logpath
+python $CMEC_CODE_DIR/drought_metrics.py -test_path $testpath -wgt_path $wgtpath -obs_path $obspath -hu_name "$region" -shp_path $shppath -out_path $outpath -interpolation $intrp >> $logpath
 
 # Make cmec outputs if drought metrics succeeds
 if [[ $? = 0 ]]; then
