bb9c9068c53e085c935ccb2c93ae73ed7a494a03
Ana Ordonez <ordonez4@llnl.gov>
2021-01-04 16:45:03 -0800

    add example outputs

---
+++ example_output/cmec_run.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+export CMEC_CODE_DIR=/export/ordonez4/git/my_forks/Drought_Metrics
+export CMEC_OBS_DATA=/export/ordonez4/git/cmec-driver/obs/Drought_Metrics
+export CMEC_MODEL_DATA=/export/ordonez4/git/cmec-driver/model/Drought_Metrics
+export CMEC_WK_DIR=/export/ordonez4/git/cmec-driver/output/Drought_Metrics
+/export/ordonez4/git/my_forks/Drought_Metrics/cmec_drought_metrics.sh
